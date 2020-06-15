314b2dc89e23f7466e3f34ee1fe66436dcf7943f
Satish <39167208+Path2Freedom@users.noreply.github.com>
2020-06-16 05:19:10 +0530

    Add files via upload

---
+++ People.Service/IPersonService.cs
@@ -0,0 +1,34 @@
+﻿using PersonRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace People.Service
+{	
+	[ServiceContract]
+	public interface IPersonService
+	{
+
+		[OperationContract]
+		List<Person> GetPeople();
+
+		[OperationContract]
+		Person GetPerson(string lastName);
+
+		[OperationContract]
+		void AddPerson(Person newPerson);
+
+		[OperationContract]
+		void UpdatePerson(string lastName, Person updatedPerson);
+
+		[OperationContract]
+		void UpdatePeople(List<Person> updatedPeople);
+
+		[OperationContract]
+		void DeletePerson(string lastName);
+	}
+}
